099840d78ebeeacccd52e5e373f090674e94594f
Pradhyumnaa Ganapathi Subramanian <p.ganapathisubramanian@student.tudelft.nl>
2019-03-31 15:51:48 +0000

    Update LeaderboardController.java

--- src/main/java/gui/controller/LeaderboardController.java
+++ src/main/java/gui/controller/LeaderboardController.java
@@ -25,10 +25,11 @@ public class LeaderboardController implements Initializable {
 		
 		XYChart.Series weeklyseries = new XYChart.Series<>();
 		
-		weeklyseries.getData().add(new XYChart.Data("1",23)); // Replace the number "23" with the server method to find CO2 consumption for that particular week.
+		weeklyseries.getData().add(new XYChart.Data("1",23)); // Replace the number "23" with the server method to find CO2 consumption for that particular week and please replace the number "1" with the server method for getting the name of friends.
 		weeklyseries.getData().add(new XYChart.Data("2",10));
 		weeklyseries.getData().add(new XYChart.Data("3",12));
 		weeklyseries.getData().add(new XYChart.Data("4",2));
+		weeklyseries.getData().add(new XYChart.Data("5",15));
 		Leaderboard.getData().addAll(weeklyseries);
 		
 	}
