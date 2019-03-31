3b98b8e0fa07010c9dcfae5a51c018c342498e6b
Danila Romanov <d.v.romanov@student.tudelft.nl>
2019-03-31 16:01:09 +0000

    Merge branch 'master' into 'bugfixesandcheckstyle'
    # Conflicts:
    #   src/main/java/entity/Feature.java
    #   src/main/java/gui/Main.java
    #   src/main/java/gui/controller/HomepageController.java
    #   src/main/java/gui/controller/MainController.java
    #   src/main/java/gui/controller/RegisterController.java
    #   src/main/java/gui/controller/VideoController.java

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -7,25 +7,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-
+   
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/application/MainFXML.fxml"));
+            URL url = new File("src/main/java/gui/fxml/MainFXML.fxml").toURL();
+            Parent root = FXMLLoader.load(url);
             Scene scene = new Scene(root);
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
             primaryStage.sizeToScene();
             primaryStage.show();
-
+      
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
