7edb87994204f7b7a7c9dd632dec7a588e12aad3
Pradhyumnaa Ganapathi Subramanian <p.ganapathisubramanian@student.tudelft.nl>
2019-04-14 13:57:28 +0000

    Update YourProgressController.java

--- src/main/java/gui/controller/YourProgressController.java
+++ src/main/java/gui/controller/YourProgressController.java
@@ -1,12 +1,20 @@
 package gui.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,6 +33,10 @@ public class YourProgressController implements Initializable {
     private LineChart<?, ?> weekly;
     @FXML
     private LineChart<?, ?> monthly;
+    @FXML
+    private Button Home;
+    @FXML
+    private AnchorPane Rootpane;
 
     @SuppressWarnings({"rawtypes", "unchecked"})
     @Override
@@ -42,6 +54,16 @@ public class YourProgressController implements Initializable {
 
         @SuppressWarnings("unused")
         XYChart.Series monthlyseries = new XYChart.Series<>();
+    }
+    @FXML
+    void clickhome(ActionEvent event) throws Exception {
+        Parent secondview;
+        @SuppressWarnings("deprecation")
+		URL url = new File("src/main/java/gui/fxml/Homepage.fxml").toURL();
+        secondview = FXMLLoader.load(url);
+        Scene newscene = new Scene(secondview);
+        Stage curstage = (Stage) Rootpane.getScene().getWindow();
+        curstage.setScene(newscene);
     }
 
 }
